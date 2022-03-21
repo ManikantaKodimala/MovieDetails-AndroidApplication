c80b09600c5f33ac11d3f059d30c03733f368052
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-03-22 01:18:28 +0530

    Add search activity

---
+++ app/src/main/java/com/example/moviedetails/SearchableActivity.kt
@@ -0,0 +1,43 @@
+package com.example.moviedetails
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviedetails.network.MovieApi
+import com.example.moviedetails.network.MovieRepository
+import com.example.moviedetails.network.RetrofitClient
+import kotlinx.android.synthetic.main.activity_searchable.*
+
+class SearchableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchable)
+
+        val searchRV = findViewById<RecyclerView>(R.id.searchResults)
+        val viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
+        val movieRepository =
+            MovieRepository(RetrofitClient.getClient().create(MovieApi::class.java))
+        searchRV.layoutManager =
+            LinearLayoutManager(this).apply { orientation = LinearLayoutManager.VERTICAL }
+
+        searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                viewModel.searchMovie(query, movieRepository)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+        viewModel.listOfSearchedMoviesByTitle.observe(this) {
+            searchRV.adapter = MovieAdapter(it)
+        }
+    }
+    
+}
+
