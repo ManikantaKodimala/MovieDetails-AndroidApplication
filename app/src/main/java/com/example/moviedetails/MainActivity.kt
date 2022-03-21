b03651f10c691d623da0a0682b14572e5329ad2c
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-03-22 01:25:32 +0530

    Add search button listern

--- app/src/main/java/com/example/moviedetails/MainActivity.kt
+++ app/src/main/java/com/example/moviedetails/MainActivity.kt
@@ -1,23 +1,34 @@
 package com.example.moviedetails
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
-import com.example.moviedetails.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val binding = ActivityMainBinding.inflate(layoutInflater)
-//        val tabLayout = binding.homeScreenTabLayout
-//        val viewPager = binding.homeScreenPager
         val tabLayout= findViewById<TabLayout>(R.id.home_screen_tab_layout)
         val viewPager =  findViewById<ViewPager>(R.id.home_screen_pager)
         val pagerAdapter = ScreenAdapter(supportFragmentManager,tabLayout.tabCount)
         viewPager.adapter=pagerAdapter
         tabLayout.setupWithViewPager(viewPager)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_search){
+            startActivity(Intent(applicationContext, SearchableActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
